ff8d365359ccd6562ee4779c394a8d1818ede54f
Purpurova-k <katrin.purpurova@gmail.com>
2022-03-17 13:11:57 +0300

    Tasks 1,2,3 done

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,44 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0; 
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+
+  min = Math.min(...arr);
+  max = Math.max(...arr);
+
+  let avg = parseFloat((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
-
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
 
-  // Ваш кода
-  // for ...
-  
+  for (let arr of arrOfArr) {
+    let sum = func(arr);
+    max = sum > max ? sum : max;
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  return Math.abs(max - min);
 }
