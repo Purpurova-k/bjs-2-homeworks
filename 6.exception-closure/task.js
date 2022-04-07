a127906b763d3e4a846b4308407e032f366fe8e1
Purpurova-k <katrin.purpurova@gmail.com>
2022-04-07 17:33:12 +0300

    Tasks 1, 2 done

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,50 @@
+// Задача 1
+function parseCount(value) {
+    let result = Number.parseInt(value, 10);
+    if (isNaN(result)) {
+        throw new Error("Невалидное значение");
+    }
+    return result;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch(error) {
+        return error;
+    }
+}
+
+
+// Задача 2
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b < c || a + c < b || b + c < a) {
+            throw new Error ("Треугольник с такими сторонами не существует");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    getPerimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    getArea() {
+        let p = this.getPerimeter() / 2;
+        return parseFloat(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch(error) {
+        return {
+            getArea: () => "Ошибка! Треугольник не существует",
+            getPerimeter: () => "Ошибка! Треугольник не существует"
+        }
+    }
+}
+
